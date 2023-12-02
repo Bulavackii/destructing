46ad18dfbc8ecb83d3bf2581f9b0336a3b8234cb
Your Name <you@example.com>
2023-12-02 12:17:19 +0300

    ready task 2

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,43 @@
+const extractSpecialAttacks = require('../app');
+
+describe('extractSpecialAttacks function', () => {
+  test('should extract special attacks with description', () => {
+    const character = {
+      special: [
+        { id: 1, name: 'Attack1', description: 'Desc1', icon: 'http://...' },
+        { id: 2, name: 'Attack2', description: 'Desc2', icon: 'http://...' },
+      ],
+    };
+
+    const result = extractSpecialAttacks(character);
+
+    expect(result).toEqual([
+      { id: 1, name: 'Attack1', description: 'Desc1', icon: 'http://...' },
+      { id: 2, name: 'Attack2', description: 'Desc2', icon: 'http://...' },
+    ]);
+  });
+
+  test('should set default description for attacks without description', () => {
+    const character = {
+      special: [
+        { id: 1, name: 'Attack1', icon: 'http://...' },
+        { id: 2, name: 'Attack2', icon: 'http://...' },
+      ],
+    };
+
+    const result = extractSpecialAttacks(character);
+
+    expect(result).toEqual([
+      { id: 1, name: 'Attack1', description: 'Описание недоступно', icon: 'http://...' },
+      { id: 2, name: 'Attack2', description: 'Описание недоступно', icon: 'http://...' },
+    ]);
+  });
+
+  test('should handle empty special attacks array', () => {
+    const character = { special: [] };
+
+    const result = extractSpecialAttacks(character);
+
+    expect(result).toEqual([]);
+  });
+});
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+function extractSpecialAttacks({ special }) {
+  return special.map(({ id, name, description = 'Описание недоступно', icon }) => ({
+    id,
+    name,
+    description,
+    icon,
+  }));
+}
+
+module.exports = extractSpecialAttacks;
